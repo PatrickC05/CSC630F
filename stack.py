8d2eece901d31ad63bc53ea28905919f7d45eb98
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2022-09-22 15:55:51 -0400

    stacks

---
+++ stack.py
@@ -0,0 +1,51 @@
+"""
+Despite coding in Python for quite a few years, I never realized that lists in python are really stacks.
+The basic functions are just renamed in the built-in python list: push vs append for adding an element,
+size vs len for getting the number of elements, and peek for getting the most recent elements.
+Pop (for removing and returning the last element) removes the same. However, it's also interesting that
+the last in first out nature is removed from python lists. In stacks, you aren't able to remove elements
+midway through the list, whereas you can in Python lists.
+"""
+
+class Stack():
+    def __init__(self):
+        self.items = []
+        self.top = None
+    
+    def push(self, item):
+        self.items.append(item)
+        self.top = item
+    
+    def pop(self):
+        l = self.items.pop()
+        self.top = self.items[-1]
+        return l
+
+    def isEmpty(self):
+        return self.size() == 0
+    
+    def peek(self):
+        return self.top
+    
+    def size(self):
+        return len(self.items)
+    
+    def __str__(self):
+        return str(self.items)
+
+if __name__ == "__main__":
+    s = Stack()
+    print(str(s.isEmpty()) + " (expected True)")
+    try:
+        print(s.pop())
+    except:
+        print("expected pop error")
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    print(str(s.size()) + " (expected 3)")
+    print(str(s) + " (expected [1, 2, 3])")
+    print(str(s.pop()) + " (expected 3)")
+    print(str(s) + " (expected [1, 2])")
+    print(str(s.peek()) + " (expected 2)")
+    print(str(s.isEmpty()) + " (expected False)")
